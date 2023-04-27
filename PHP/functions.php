0bcc3abcd2e3457191c523ab096399e613947fdd
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-04-27 13:14:14 +0200

    Added function sendJSON and saveToFile
    These two functions will be used repeatedly.

---
+++ PHP/functions.php
@@ -0,0 +1,13 @@
+<?php
+function sendJSON($data, $statusCode = 200) {
+    header("Content-Type: application/json");
+    http_response_code($statusCode);
+    $json = json_encode($data);
+    echo $json;
+    exit();
+function saveToFile($file, $array) {
+    $json = json_encode($array, JSON_PRETTY_PRINT);
+    file_put_contents($file, $json);
+}
+}
+?>
