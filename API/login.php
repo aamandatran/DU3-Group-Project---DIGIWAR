6bbb9a6b8d868698f0654e1ee75a737dcc6d8f1d
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-04-27 13:55:51 +0200

    login.php done

---
+++ API/login.php
@@ -0,0 +1,52 @@
+<?php
+ini_set('display_errors', 1);
+require_once("functions.php");
+
+$filename = "users.json";
+$method = $_SERVER["REQUEST_METHOD"];
+
+if($method == "POST") {
+
+    $dataJSON = file_get_contents("php://input");
+    $data = json_decode($dataJSON, true);
+
+    $username = $data["username"];
+    $password = $data["password"];
+
+    $usersJSON = file_get_contents($filename);
+    $users = json_decode($usersJSON, true);
+
+    if($username == "" or $password == "") {
+        $error = [
+            "message" => "Empty values, please write a username and password"
+        ];
+        sendJSON($error, 400);
+    }
+
+    for($i = 0; $i < count($users); $i++) {
+        if($users[$i]["username"] == $username and $users[$i]["password"] == $password){
+            sendJSON($users[$i]);
+        } else if($users[$i]["username"] == $username and $users[$i]["password"] != $password) {
+            $error = [
+                "message" => "Password is incorrect"
+            ];
+            sendJSON($error, 400);
+        } else {
+            $userfound = false;
+        }
+    }
+
+    if(!$userfound) {
+        $error = [
+            "message" => "Not found"
+        ];
+        sendJSON($error, 404);
+    }
+
+} else {
+    $error = [
+        "message" => "Only POST works."
+    ];
+    sendJSON($error, 400);
+}
+?>
