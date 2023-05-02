14723ab4904a67d31fbc60096c9d29d09ee30804
Wille2002 <wilhelm.sjostedtthiel@gmail.com>
2023-05-02 12:15:28 +0200

    Added some explination to the code

--- API/edit_profile.php
+++ API/edit_profile.php
@@ -7,30 +7,61 @@ $method=$_SERVER["REQUEST_METHOD"];
 $contentType=$_SERVER["CONTENT_TYPE"];
 
 if ($method!=="PATCH") {
+    //Kollar så att det är rätt metod som andvänds. 
     $error=[
         "message"=>"Only PATCH works."
     ];
     sendJSON($error,400);
 }
 if ($contentType!=="application/json") {
+    //Kollar så att det är rätt content type
     $error=[
         "message"=>"only JSON works."
     ];
     sendJSON($error,400);
 }
 
-$dataJSON= file_get_contents("php://input");
-$data=json_decode($dataJSON,true);
+$data=getFileContents("php://input");
 
 $oldPassword=$data["oldPassword"];
 $newPassword=$data["newPassword"];
+$profilePicture=$data["profilepicture"];
+//Hämtar informationen från requesten. 
 
-$usersJSON=file_get_contents($filename);
-$users=json_decode($usersJSON,true);
+$users=getFileContents($filename);
+//Hämtar all information från json filen. 
+
+if ($newPassword==="" or $oldPassword==="") {
+    //Kollar så att fälten är ifyllda
+    $error=[
+        "message"=>"Empty values"
+    ];
+    sendJSON($error,400);
+}elseif (strlen($newPassword<3)) {
+    //Kollar så att man inte skrivit i ett för kort lösenord. 
+    $error=[
+        "message"=>"New password needs to be atleast 3 characters"
+    ];
+    sendJSON($error,400);
+}else{
+    foreach($users as $user){
+        if ($user["password"]===$newPassword) {
+            //Går igenom alla andvändarses lösenord för att kontrollera så att man inte tar ett befintligt lösenord. 
+           $error=[
+            "message"=>"Password is already taken"
+           ];
+           sendJSON($error,409);
+        }
+    }
+}
 
 foreach ($users as $user) {
   if ($oldPassword===$user["password"]) {
+    //Här går vi igenom alla andvändares lösenord och hittar rätt andvändar med hjälp av det gamla lösenordet. 
     $user["password"]=$newPassword;
+    //När vi hittat rätt andvändare så bytar vi ut det nya mot det gamla. 
+    $user["profilePicture"]=$profilePicture;
+    //Vi bytar ut den selecterade profilbilden mot den gamla. 
     break;
   }
 }
@@ -38,7 +69,7 @@ foreach ($users as $user) {
 saveToFile($filename,$users);
 
 $response=[
-    "message"=>"password updated succesfully!"
+    "message"=>"Profile updated succesfully!"
 ];
 sendJSON($response)
 ?>
