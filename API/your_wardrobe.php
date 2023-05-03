59378cda912f983262d04fdde41df2d42dfb37b9
tranenn <aamandatran@gmail.com>
2023-05-03 13:01:52 +0200

    Added  response if method is GET and DELETE
    If method is GET, the response will be an object with arrays containing tops, bottoms, shoes.
    
    If method is DELETE, the item will be removed from the array.

--- API/your_wardrobe.php
+++ API/your_wardrobe.php
@@ -35,14 +35,17 @@ if(isset($_FILES["item"])) {
         if($_POST["file"] == "tops.json") {
             $tops[] = $newItem;
             saveToFile("tops.json", $tops);
+            $message["file"] = "tops.json";
             sendJSON($message, 200);
         } elseif($_POST["file"] == "bottoms.json") {
             $bottoms[] = $newItem;
             saveToFile("bottoms.json", $bottoms);
+            $message["file"] = "bottoms.json";
             sendJSON($message, 200);
         } elseif($_POST["file"] == "shoes.json") {
             $shoes[] = $newItem;
             saveToFile("shoes.json", $shoes);
+            $message["file"] = "shoes.json";
             sendJSON($message, 200);
         } else{
             $message = [
@@ -59,5 +62,52 @@ if(isset($_FILES["item"])) {
     } 
 }
 
+// Sends back every item to display on website
+if($requestMethod == "GET") {
+    $wardrobe = [
+        "tops" => $tops,
+        "bottoms" => $bottoms,
+        "shoes" => $shoes
+    ];
+
+    sendJSON($wardrobe, 200);
+}
+
+
+if($requestMethod == "DELETE") {
+    $path = $requestData["path"];
+    $JSONfile = $requestData["file"];
+
+    // Check which JSON file to iterate over and search for the item
+    if($JSONfile == "tops.json") {
+        $items =& $tops;
+    } elseif($JSONfile == "bottoms.json") {
+        $items =& $bottoms;
+    } elseif($JSONfile == "shoes.json") {
+        $items =& $shoes;
+    } else {
+        // If something went wrong, for example if no file was sent
+        $message = ["message" => "Something went wrong... please try again!"];
+        sendJSON($message, 418);
+    }
+
+    foreach($items as $key => $item) {
+        if($path == $item["path"]) {
+            // Remove the item from the array
+            unset($items[$key]);
+
+            // Save the updated array back to the file
+            saveToFile($JSONfile, $items);
+
+            // Send a success response
+            $message = [
+                "message" => "The item has been deleted successfully!",
+                "ok" => true
+            ];
+            sendJSON($message, 200);
+        }
+    }
+
+}
 
 ?>
