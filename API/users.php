3a66223fed15dddb3394d619ff41d2cbf9c50f3b
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-05-04 13:08:20 +0200

    Create users.php
    För att fetcha användare kan vara bra till edit_profile eller hitta id i js

---
+++ API/users.php
@@ -0,0 +1,13 @@
+<?php
+ini_set("display_errors", 1);
+require_once("functions.php");
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+//Används när man vill nå arrayen med våra utvalda profilbilder
+if($method == "GET") {
+    $users = getFileContents("users.json");
+    sendJSON($users);
+}
+
+?>
