269190ad1befd21717ec76a22817e55575ddcba0
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-05-10 10:24:38 +0200

    Changes

--- JS/register.js
+++ JS/register.js
@@ -41,7 +41,7 @@ async function renderRegisterPage() {
     }
 
     main.innerHTML = `
-    <div id = parent>
+    <div id = registerParent>
         <div id = register>
             <div id = LoginRegisterContainer> 
                 <button id = LoginButton>SIGN IN</button>
