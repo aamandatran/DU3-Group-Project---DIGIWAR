b2e85c4bdc2bdc95da902db55a7f02889f470a72
tranenn <aamandatran@gmail.com>
2023-05-10 10:12:04 +0200

    Problem with merge

--- JS/login.js
+++ JS/login.js
@@ -3,18 +3,18 @@
 function renderLoginPage() {
     main.innerHTML = `
     <div id=LoginRegisterContainer> 
-    <button id=LoginButton>SIGN IN</button>
-    <button id=RegisterButton>JOIN</button>
+        <button id=LoginButton>SIGN IN</button>
+        <button id=RegisterButton>JOIN</button>
     </div>
 
     <form>
-    <p class=InputHeader>Username</p>
-    <input type=text placeholder=Username id=username>
-    <p class=InputHeader>Password</p>
-    <input type=password placeholder=Password id=password>
-    <div id=submitButtonContainer>
-    <button id=submitButton type=submit>Join</button>
-    </div>
+        <p class=InputHeader>Username</p>
+        <input type=text placeholder=Username id=username>
+        <p class=InputHeader>Password</p>
+        <input type=password placeholder=Password id=password>
+        <div id=submitButtonContainer>
+            <button id=submitButton type=submit>Join</button>
+        </div>
     </form>
     
     `
