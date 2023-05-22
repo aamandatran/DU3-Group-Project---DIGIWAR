519701c71455587233e68937be4727ada15d51fa
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-05-22 22:22:54 +0200

    Added some colors to colorArray

--- JS/new_outfit.js
+++ JS/new_outfit.js
@@ -18,7 +18,7 @@ function randomColor(event) {
   console.log("hejdå");
   let backgroundDiv = document.getElementById("popupSelectedItems");
 
-  let colorsArray = ["#ffe4e9", "#ceeaec", "#cfe1c8", "black", "white", "#dbd0e3", "#fffbdb"];
+  let colorsArray = ["#ffe4e9", "#ceeaec", "#cfe1c8", "black", "#f6efef", "#dbd0e3", "#fffbdb", "#f9c8d0", "#f1e0c9"];
   let chosenColor = colorsArray[Math.floor(Math.random() * colorsArray.length)]
 
   backgroundDiv.style.backgroundColor = chosenColor;
