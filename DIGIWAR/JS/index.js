f492ab3cd797e8ee22f0d431dedb910eb220e79f
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-05-30 23:01:10 +0200

    Put filterarray in index.js

--- DIGIWAR/JS/index.js
+++ DIGIWAR/JS/index.js
@@ -1,6 +1,8 @@
 "use strict";
 let main = document.querySelector("main");
 
+let filterArray = ["all", "streetwear", "casual", "sporty", "formal", "business", "party", "summer", "winter", "spring", "autumn"];
+
 //If localstorage item isLoggedIn is true it will render wardrobe page
 if (window.localStorage.getItem("isLoggedIn")) {
     renderWardrobePage();
